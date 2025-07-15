b5d23b93f5afe1be03891dfe1ae04a7af47f76ff
scorpion official x <168276307+scorpionOfficialX@users.noreply.github.com>
2025-07-16 04:41:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ load_dotenv()
 # 移除默认的日志处理器
 logger.remove()
 # 添加标准输出
-print("从配置文件中读取到的DEBUG为", settings.app.debug)
+print("DEBUG", settings.app.debug)
 handler_id = logger.add(sys.stderr, level="INFO" if not settings.app.debug else "DEBUG")
 # 添加文件写出
 logger.add(
